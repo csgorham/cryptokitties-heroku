613bd394df4174b14f519eb1c05e0e6503aad3ed
csgorham <caroline.gorham@gmail.com>
2021-11-28 14:16:53 -0500

    1

--- nft2.py
+++ nft2.py
@@ -159,12 +159,12 @@ if endpoint == 'Price Arbitrage Downloads':
 	#asset = modelresults.iloc[idx]
 	#render_asset(asset)
 	
-	if pricing == 'Overpriced' & virginity == 'Virgin':
+	if pricing == 'Overpriced' && virginity == 'Virgin':
 		st.markdown(get_table_download_link_csv(virgins_overpriced), unsafe_allow_html=True)
-	elif pricing == 'Underpriced' & virginity == 'Virgin':
+	elif pricing == 'Underpriced' && virginity == 'Virgin':
 		st.markdown(get_table_download_link_csv(virgins_underpriced), unsafe_allow_html=True)
-	elif pricing == 'Underpriced' & virginity == 'Non-Virgin':
+	elif pricing == 'Underpriced' && virginity == 'Non-Virgin':
 		st.markdown(get_table_download_link_csv(nonvirgins_underpriced), unsafe_allow_html=True)
-	elif pricing == 'Overpriced' & virginity == 'Non-Virgin':
+	elif pricing == 'Overpriced' && virginity == 'Non-Virgin':
 		st.markdown(get_table_download_link_csv(nonvirgins_overpriced), unsafe_allow_html=True)
 
