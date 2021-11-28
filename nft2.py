a70c21e588ec8a6a13fb9014b0f945d7e23a7b7b
csgorham <caroline.gorham@gmail.com>
2021-11-28 13:28:10 -0500

    1

--- nft2.py
+++ nft2.py
@@ -124,10 +124,9 @@ if endpoint == 'Cattribute Results':
 		st.image('./graphics/nonvirgins_pattern_r2.png')
 
 def get_table_download_link_csv(df):
-    #csv = df.to_csv(index=False)
-    csv = df.to_csv().encode()
+    csv = df.to_csv(index=False)
     #b64 = base64.b64encode(csv.encode()).decode() 
-    b64 = base64.b64encode(csv).decode()
+    b64 = base64.b64encode(csv.encode()).decode()
 
 
     href = f'<a href="data:file/csv;base64,{b64}" download={df.name} +".csv" target="_blank">Download {df.name} csv file</a>'
